3145ac71bbc056178190d931f48cf94756132ed5
pinzauti <pinzautid@gmail.com>
2021-12-29 15:28:17 +0100

    configurato firebase firestore e iniziata login

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyCT7vj79ya-ET8qyl7QlLzKlpIIYQ-ibqM",
+  authDomain: "pulsantieramegafotonica-dbd1e.firebaseapp.com",
+  projectId: "pulsantieramegafotonica-dbd1e",
+  storageBucket: "pulsantieramegafotonica-dbd1e.appspot.com",
+  messagingSenderId: "1092690990323",
+  appId: "1:1092690990323:web:49d2ffcc9ea1201ec1d386",
+  measurementId: "G-NKV8JNG846",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,44 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
+import Dashboard from "../components/Dashboard.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/login",
+    name: "login",
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
